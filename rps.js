6180daaa747bc593558d4d26b6f0fa3a39ad17d6
Nika Calapini <nmc2003@nmsu.edu>
2023-06-08 00:15:01 -0700

    incorporated DOM

--- rps.js
+++ rps.js
@@ -20,29 +20,86 @@ function getPlayerChoice(){
 let userScore = 0;
 let computerScore = 0;
 
+const output = document.getElementById('output');
+
 function playRound (playerSelection, computerSelection){
+    
     if (computerSelection === playerSelection){
-        return "It's a tie!";
+        output.textContent = "It's a tie!";
     }
     else if (computerSelection === "rock" && playerSelection === "paper"){
         userScore++;
-        return "You win! Paper beats Rock";
+        output.textContent = "You win! Paper beats Rock";
     }
     else if (computerSelection === "paper" && playerSelection === "scissors"){
         userScore++;
-
-        return "You win! Scissors beats Paper";
+        output.textContent = "You win! Scissors beats Paper";
     }
     else if (computerSelection === "scissors" && playerSelection === "rock"){
         userScore++;
-        return "You win! Rock beats Scissors";
+        output.textContent = "You win! Rock beats Scissors";
     }
     else{
         computerScore++;
-        return `You lose, ${computerSelection} beats ${playerSelection}!`;
+        output.textContent = `You lose, ${computerSelection} beats ${playerSelection}!`;
     }
 }
 
+const rockButton = document.getElementById('rockButton');
+const paperButton = document.getElementById('paperButton');
+const scissorButton = document.getElementById('scissorButton');
+
+rockButton.addEventListener('click',() => buttonClicked('rock'));
+paperButton.addEventListener('click',() => buttonClicked('paper'));
+scissorButton.addEventListener('click',() => buttonClicked('scissors'));
+
+function buttonClicked(playerSelection) {
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    scoreTracker();
+    if (userScore === 5 || computerScore === 5) {
+        gameWinner();
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorButton.disabled = true;
+    }
+  }
+
+function scoreTracker(){
+    const myScore = document.getElementById('userScore');
+    myScore.textContent = `Your Score: ${userScore}`;
+    const compScore = document.getElementById('computerScore');
+    compScore.textContent = `Computer's Score: ${computerScore}`;
+}
+
+const overallWinner = document.getElementById('gameWinner');
+
+function gameWinner(){
+    
+    if (computerScore===5){
+        overallWinner.textContent = "Game Over! YOU LOST! Press button to play again."; 
+    }
+    else if (userScore === 5){
+       overallWinner.textContent = "Game Over! YOU WON! Press button to play again.";
+    }
+}
+
+const playAgainButton = document.getElementById('playAgainButton');
+playAgainButton.addEventListener('click',playAgain);
+
+function playAgain() {
+    userScore = 0;
+    computerScore = 0;
+    scoreTracker();
+    overallWinner.textContent = "";
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorButton.disabled = false;
+
+  }
+
+
+/*
 function game(){
     for (let i = 0; i < 5; i++){
         const playerSelection = getPlayerChoice();
@@ -52,12 +109,9 @@ function game(){
         console.log("Computer's Score: " + computerScore);
     }
 
-    if (computerScore>userScore){
-        console.log("You lost the game!");
-    }
-    else{
-        console.log("You won the game!");
-    }
+   
 }
     
 console.log(game());
+
+*/
